805e294ec430e5e9f0bf16ef3020708561b9ec0f
MerveYaras <54952604+MerveYaras@users.noreply.github.com>
2020-10-08 12:12:04 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        ArrayList list = new ArrayList();
+       
+        private void button1_Click(object sender, EventArgs e)
+        {  //Kaydet Butonu
+            Kisi kisi = new Kisi();//bir instance'dır. Ram üzerinde oluşturulan kopyadır. 
+            kisi.Ad = textBox1.Text;
+            kisi.SoyAd = textBox2.Text;
+            kisi.Meslek = textBox3.Text;
+            kisi.Tarih = dateTimePicker1.Value;
+            kisi.EvliMi= checkBox1.Checked;
+
+            list.Add(kisi);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Yenile butonu
+            listBox1.Items.Clear();
+            foreach (var item in list)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Detay butonu
+            Kisi deger = (Kisi)listBox1.SelectedItem;
+            label5.Text = deger.Ad;
+            
+        }
+    }
+}
---
+++ Kisi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassUygulama
+{
+    class Kisi
+    {
+        
+
+
+        string _ad;
+        public string Ad
+        {
+            get { return _ad; }
+            set { _ad = value; }
+        }
+
+        string _soyAd;
+        public string SoyAd
+        {
+            get { return _soyAd; }
+            set { _soyAd = value; }
+        }
+
+        string _meslek;
+        public string Meslek
+        {
+            get { return _meslek; }
+            set { _meslek = value; }
+        }
+
+        DateTime _tarih;
+        public DateTime Tarih
+        {
+            get { return _tarih; }
+            set { _tarih = value; }
+        }
+
+        bool _evliMi;
+        public bool EvliMi
+        {
+            get { return _evliMi; }
+            set { _evliMi = value; }
+        }
+
+        public override string ToString()
+        {
+            //bir metodu tekrar tanımlar.
+            return _ad +","+ _soyAd +","+ _meslek +","+_tarih +","+ _evliMi;
+        }
+
+        public string TamAd
+        {
+            get { return _ad + _soyAd; }
+        }
+
+    }
+}
